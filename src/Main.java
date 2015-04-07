6908b8978e052be5dfd70cd54970f40087e152be
hajjboy95 <gizmo-boy95@hotmail.com>
2015-04-07 18:16:25 +0100

    added Jreddit to get submission and comments

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+import com.github.jreddit.entity.Submission;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		 System.out.println("BEGIN RUN");
+		
+		 //test account , and password
+		 Reddit reddit = new Reddit("izzy19959","u7f-ozz-nkv-Gkq");
+		 List<Submission> submissions =  reddit.getSubmission(1); //returns 1 submission
+
+		 for (Submission sub : submissions) {
+			 System.out.println("BEGINNING OF SUBMISSION \n"+sub);
+			 System.out.println(reddit.getCommentsForSubmission(sub.getIdentifier()));  //prints out
+			 System.out.println("END OF SUBMISSION \n");
+		 }
+		 
+		 
+		 System.out.println("END RUN");
+
+	}
+
+}
