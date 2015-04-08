fd46dac51b01423cde31fe8db7d81cd9d3cf372e
Diarmuid <diarmuidr3d@gmail.com>
2015-04-08 16:49:08 +0100

    Remade for Maven

---
+++ src/main/java/reddit/Main.java
@@ -0,0 +1,31 @@
+package reddit;
+
+import com.github.jreddit.entity.Comment;
+import com.github.jreddit.entity.Submission;
+import java.util.List;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("BEGIN RUN");
+
+		//test account , and password
+		Reddit reddit = new Reddit("izzy19959","u7f-ozz-nkv-Gkq");
+		List<Submission> submissions =  reddit.getSubmission(1); //returns 1 submission
+
+		for (Submission sub : submissions) {
+			System.out.println("SUBMISSION TITLE :"+sub.getTitle() + "\n\n" + "COMMENTS:\n");
+			String id = sub.getIdentifier();
+			for (Comment c : reddit.getCommentsForSubmission(id)){
+				System.out.println(c.getBody());
+			}
+			System.out.println();
+		}
+
+
+		System.out.println("END RUN");
+
+	}
+
+}
