1254845143b601c5aa0aa14a55dfdd401ef2b16a
hajjboy95 <gizmo-boy95@hotmail.com>
2015-04-16 14:55:03 +0100

    added Ulys code  to Main

--- src/main/java/CIDR/HuesNews/App.java
+++ src/main/java/CIDR/HuesNews/App.java
@@ -1,51 +1,64 @@
 package CIDR.HuesNews;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Stack;
-import java.util.Timer;
-import java.util.TimerTask;
-import languageGenerator.CreateSentence;
 import com.github.jreddit.entity.Submission;
+import fileReader.TextBot;
+import languageGenerator.CreateSentence;
 import reddit.Reddit;
-import stanfordParser.*;
-import twitter4j.TwitterException;
+import stanfordParser.FrequencyStack;
+import stanfordParser.Parser;
+import stanfordParser.Token;
+
+import java.util.*;
 
 public class App 
 {
 	public static void main( String[] args )
 	{
 		final Timer tickTock = new Timer();  // Create a Timer object
-		
+		final  String TARGET = "ULYSSES.txt";
+
 		Reddit reddit = new Reddit("izzy19959","u7f-ozz-nkv-Gkq");        
 		Parser stanParse = new Parser();
-		CreateSentence simpNlG = new CreateSentence();
 		FrequencyStack fs = new FrequencyStack();
 		TwitterBot CIDRBOT = new TwitterBot();
 		CreateSentence sentenceGenerator = new CreateSentence();
+		TextBot textbot = new TextBot(TARGET);
 		
 		
 		List<Submission> submissions =  reddit.getSubmission(1); //returns 1 submission
 		String headline = submissions.get(0).getTitle();
-		ArrayList<Token> tokens = stanParse.parse(headline);
-		HashMap<String, Stack<Token>> sortedList = fs.sortList(tokens);
-		System.out.println(sortedList);
-		System.out.println(headline);
-		String generatedSentence = sentenceGenerator.process(tokens, sortedList);
-		System.out.println(generatedSentence);
+
+
+		ArrayList<Token> tokenHeadline = stanParse.parse(headline);
+		ArrayList<Token> tokenUly = stanParse.parse(textbot.getRandom());
+
+		HashMap<String, Stack<Token>> sortedListHeadline = fs.sortList(tokenHeadline);
+
+		HashMap<String, Stack<Token>> sortedListUly = fs.sortList(tokenUly);
+
+
+		String generatedSentenceFromHeadline = sentenceGenerator.process(tokenHeadline, sortedListHeadline);
+		String generatedSentenceFromUly = sentenceGenerator.process(tokenHeadline, sortedListHeadline);
+
+		System.out.println(sortedListHeadline);
+		System.out.println(sortedListUly);
+
+		System.out.println("generated Headline = " + generatedSentenceFromHeadline);
+		System.out.println("generated From Uly = " + generatedSentenceFromUly);
+
+
 		
-		
+		//need keys in TwitterBot class to generate Tweets
 //		try {
-//			CIDRBOT.tweet(generatedSentence);
+//			CIDRBOT.tweet();
 //		} catch (TwitterException | IOException e) {
 //			e.printStackTrace();
-//		} 
+//		}
 		
 
 
 
+		//Will be used later.
 //		final TimerTask tickTockTask = new TimerTask(){
 //
 //
